6f8f2ca4eadb5a49aff916055dcb09dc97885e2d
Fucho <fucho@me.com>
2015-02-25 09:48:49 +0100

    Primeros intentos de programar con NetBeans con C

---
+++ NB/p03/main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ * Author: fucho
+ *
+ * Created on 25 de febrero de 2015, 9:46
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
