ee526d0e3ad6367fb7a3fd9b0caf43750c897af4
Fucho <fucho@me.com>
2015-03-03 21:26:04 +0100

    Primero pinitos con la práctica 3 de Arq2

---
+++ Practica3/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+
+enum hip
+{
+	0,1,2
+};
+
+enum smed
+{
+	0,1,2
+};
+
+enum tarj
+{
+	0,1,2
+};
+
+enum seg
+{
+	0,1,2
+};
+
+enum nat
+{
+	0,1,2
+};
+
+
+struct cuenta_corriente 
+{
+    char * titular;
+    long saldo;
+    hip hipoteca;
+    smed saldoMedio;
+    tarj tarjeta;
+    seg seguros;
+    nat naturaleza;
+};
