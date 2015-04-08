352d1aa02450b2b19ed7e13b300482d84e1bc103
luciappardo <100068468@alumnos.uc3m.es>
2015-04-08 12:41:22 +0200

    Me desespero!!!
    Mensaje al ejecutar:
    
    monitor01:~/ARQUI2_2015/P4> ./p4_1 Hola que tal va todo por aqui muy
    bien como siempre
    MAIN ->Error en mq_open
    mq_open: Invalid argument
    
    No se porque sale esto...
    La función de productor aún esta comentada porque total la cola o se
    crea...

--- Practica4/p4ej1.c
+++ Practica4/p4ej1.c
@@ -6,8 +6,14 @@ Práctica 4
 Ejercicio 1
 Uso de colas de mensajes para comunicación entre procesos
 Arquitectura de Sistemas II
-Esquema Padre - Hijo - Hijo -Hijo
+Esquema Padre(Productor) - Hijo(Consumidor)
 Mensajes de depuración disponibles con -DDEBUG
+
+Línea de compilacion sin mensajes de depuración:
+ gcc -Wall -lrt -g -o p4_1 p4ej1.c
+
+Línea de compilación con mensajes de depuración:
+ gcc -Wall -DDEBUG -lrt -g -o p4_1 p4ej1.c
 */
 
 /* Librerias */
@@ -24,29 +30,31 @@ Mensajes de depuración disponibles con -DDEBUG
 #include <fcntl.h>
 #include <wait.h>
 #include <mqueue.h>
+#include <errno.h> //Lo incluyo para saber porque mq_open=-1
 
 /* Constantes */
 
-#define MAX_MESSAGE_SIZE 11
+#define MAX_MESSAGE_SIZE 12
 
 /*Estructuras*/
 
 /* Variables globales */
-
+mqd_t mqd_q;
+	
 /* Prototipos de funciones */
-void productor();
+void productor(char * buffer);
 void consumidor();
 
 /*Funcion Productor() envia mensajes a la cola*/
-void productor(char buffer[MAX_MESSAGE_SIZE]){
+/*void productor(char * buffer){
 	#if DEBUG
 		printf("PRODUCTOR -> Acabamos de entrar en la funcion productor()\n");
 	#endif
 
-	if(mq_send(mqd,buffer,sizeof(20*char),0)==-1)
+	if(mq_send(mqd,buffer,20*sizeof(char),0)==-1)
 		printf("PRODUCTOR -> Error en mq_send\n");
 
-}
+}*/
 
 /*Funcion Consumidor() recibe mensajes de la cola*/
 void consumidor(){
@@ -59,16 +67,18 @@ void consumidor(){
 /* Main */
 int main (int argc, char *argv[]){
 
-	mqd_t mqd;
 	struct mq_attr atributos;
-	char palabras[MAX_MESSAGE_SIZE];
+	char * palabras[MAX_MESSAGE_SIZE];
 	int i;
 
 	atributos.mq_maxmsg = MAX_MESSAGE_SIZE;
-	atributos.mq_msgsize = sizeof(20*char);
+	atributos.mq_msgsize = 30*sizeof(char);
 
-	if ((mqd = mq_open("comq",O_CREAT, 0777, &atributos))==-1){
+	mqd_q = mq_open("/comq",O_CREAT, 0777, &atributos);
+
+	if (mqd_q==-1){
 		printf("MAIN ->Error en mq_open\n");
+		perror("mq_open");
 		exit(-1);
 	}
 
@@ -76,18 +86,19 @@ int main (int argc, char *argv[]){
 		printf("MAIN -> Se ha creado la cola de mensajes de nombre comq\n");
 	#endif
 
-	if(argc!=2 || strcmp(argv[1], "--help")==0){
-		usageErr("%s mq-name\n", argv[0]);
+	if(argc<2 || strcmp(argv[1], "--help")==0){
+		printf("Formato incorrecto, ./ejecutable palabras_separadas_por_espacios\n");
+		exit(-1);
 	}else{
 		printf("PALABRAS = { ");
-		for(i=0; i<MAX_MESSAGE_SIZE; i++){
+		for(i=1; i<MAX_MESSAGE_SIZE; i++){
 			palabras[i]=argv[i];
-			printf(palabras[i]);
+			printf("%s",palabras[i]);
 			if(i!=(MAX_MESSAGE_SIZE-1)){
-				printf(",");
+				printf(", ");
 			}
 		}
-		printf("}\n");
+		printf(" }\n");
 	}
 
 	pid_t pid, parent;
@@ -125,13 +136,17 @@ int main (int argc, char *argv[]){
 			
 		/* Soy el padre */
 		#if DEBUG
-			printf("MAIN -> Soy el padre y ya he creado a mi hijo (PID %d)\n", getpid());
+			printf("MAIN -> Soy el padre (Productor) y ya he creado a mi hijo (PID %d)\n", getpid());
 		#endif
 					
 		pid = getpid();
-		printf("MAIN -> Soy el padre (Productor)(PID %d)\n",pid );
-		productor(palabras);
+		printf("MAIN -> Soy el Productor (PID %d)\n",pid);
+
+		#if DEBUG
+			printf("MAIN -> Lanzo mi funcion Productor (PID %d)\n",pid);
+		#endif
+		//productor(*palabras);
 		exit(0);	
-	}	
+	
 	return 0;
 }
