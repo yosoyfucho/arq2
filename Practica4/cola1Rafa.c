b4c7c9f685941b03b7dfef1bded1294640050aff
Fucho <fucho@me.com>
2015-04-15 21:20:09 +0200

    Práctica 1 - Ej 4 funcionando
    Reseñable que la cola es el primer parámetro de la linea

--- Practica4/cola1Rafa.c
+++ Practica4/cola1Rafa.c
@@ -1,5 +1,4 @@
-
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,121 +8,134 @@
 #include <error.h>
 #include <errno.h>
 
+
 #define MAX_MESSAGE_SIZE 100
-#define MAX_QUEUE_SIZE 42
+#define MAX_QUEUE_SIZE 2
+#define PERM_ALL 0600
 
-int main(int argc, char *argv[]) 
+
+
+
+int main (int argc, char * argv[])
 {
-	int i,flags,rc;
-	mqd_t mqd;
-	flags = O_RDWR|O_CREAT;
-	char * nombreCola = "/estaCola2";
-			char * buffer = (char *)malloc(MAX_MESSAGE_SIZE);
+  if (argc <= 2 || strcmp(argv[1], "--help") == 0)
+      {
+        #if DEBUG
+          printf("MAIN -> O no hay parametros suficientes o piden ayuda para iniciar\n");
+        #endif
+        printf("main4_1.c El formato de ejecucion es:\n");
+        printf("  <./Ejecutable> <cola que abriremos con /> <mensaje compuesto por variables palabras>\n");
+        exit(-1);
+      }
 
+  int flags;
+  //mode_t perms; tercer argumento mq_open
+  mqd_t mqd;
+  struct mq_attr attr;
+  //char * name = "/cola12";
 
-#if DEBUG	
-printf("El valor de Argc es :%d \n",argc);
-printf("El nombre de la cola es %s:\n",nombreCola);
-#endif
+  attr.mq_maxmsg = MAX_QUEUE_SIZE;
+  attr.mq_msgsize = MAX_MESSAGE_SIZE;
+  flags = O_RDWR | O_CREAT;
 
-if(argc<2)
-{
-	printf("El Formato de ejecución es /nombreCola mensaje_con_espacios\n");
-	exit(-1);
-}
-
-
-	printf("Cola a usar: %s\n", nombreCola);
-	printf(" ");
-
-
-
-	printf("MENSAJE = {");
-	for (i = 1; i<argc;i++)
-	{
-		printf("%s, ", argv[i]);
-	}
-	printf("}\n");
+  mqd = mq_open(argv[1], flags, 0777, &attr);
 
 #if DEBUG
-	printf("MAIN-> Voy a abrir la cola %s\n",nombreCola);
+  printf("MAIN -> Voy a abrir la cola %s\n", argv[1]);
 #endif
-
-	mqd = mq_open(nombreCola,flags,0777,NULL);
-
-	if (mqd <0)
-	{
-		printf("Error en mq_open\n");	
-		mq_unlink(nombreCola);
-		mq_close(mqd);
-
-	}
-
-	#if DEBUG
-		printf("MAIN -> Cola creada sin errores\n");
-	#endif
-	int pid, ppid;
-	pid = fork();
-	if (pid == 0)
-	{
-		pid = getpid();
-		ppid = getppid();
-		#if DEBUG
-			printf("Soy el proceso Consumidor pid: %d y mi padre es :%d \n",pid,ppid );
-		#endif
-
-		/*Soy el hijo */
-  	int recibidos = 1;     
-    while (recibidos > 0)
+ // mqd = mq_open(argv[1],O_CREAT|O_RDWR,PERM_ALL, &atributos);
+ if (mqd == (mqd_t)-1)
+  {
+    #if DEBUG
+      printf("MAIN -> Se ha producido un error en mq_open\n");
+      printf("	Oh dear, something went wrong with mq_open()! %s\n", strerror(errno));
+    #endif
+    exit(-1);
+  }
+  #if DEBUG
+    printf("MAIN -> Cola %s creada", argv[1]);
+    printf("MAIN -> Mensaje a enviar:\n");
+    int i;
+    for (i=1;i<argc;i++)
     {
-    	//hijo, consumidor
-    	
-    	recibidos = mq_receive(mqd, buffer, MAX_MESSAGE_SIZE, 0);
-    	#if DEBUG
-    		printf("---Consumidor: Hemos recibido %d bytes\n",recibidos);
-    	#endif
-    	printf("---Consumidor: %s\n", buffer);
-    	sleep(1);
-    	memset(buffer, 0, sizeof(buffer));
+      printf("%s \n",argv[i]);
     }
-		#if DEBUG
-			printf("Consumidor -> Cierro mi descriptor de la cola\n");
-		#endif
-		mq_unlink(nombreCola);
-		mq_close(mqd);
-		free(buffer);
-		
-	}
-	else
-	{
-		pid = getpid();
-		#if DEBUG
-			printf("Soy el proceso productor y mi pid es %d\n",pid );
-		#endif
-		for (i = 1; i < argc; i++)
-		{
-			rc = mq_send(mqd,argv[i],MAX_MESSAGE_SIZE,0);
-			if (rc<0)
-			{
-				printf("Productor -> Error en mq_send()\n");
-				mq_unlink(nombreCola);
-				mq_close(mqd);
-				exit(-1);
-			}
-			#if DEBUG
-				printf("Productor: Envio con exito\n");
-			#endif
-			printf("Productor: %s\n", argv[i]);
-			sleep(1);
-		}
-		#if DEBUG
-			printf("Productor -> Cierro mi descriptor de la cola\n");
-		#endif
-		mq_unlink(nombreCola);
-		mq_close(mqd);
-	}
+  #endif
+  printf("QUEUE = %s\n", argv[1]);
+  printf("PALABRAS = { ");
+  for (i=2; i<argc-1;i++)
+  {
+    printf("%s ,",argv[i]);
+  }
+  printf("%s }\n\n",argv[argc-1]);
 
-	
-  return 0;
-} 
 
+  int pid = fork();
+  if (pid == (pid_t) 0)
+  {
+    /* Soy el hijo - Consumer*/
+    #if DEBUG
+      printf("Consumidor pid %d y padre con pid %d\n",getpid(),getppid());
+    #endif
+    int aux = 1;
+    char * buffer = (char*)malloc(sizeof(MAX_MESSAGE_SIZE));
+    while (aux > 0)
+    {
+      aux = mq_receive(mqd,buffer,MAX_MESSAGE_SIZE,0);
+      if(aux == -1)
+      {
+        #if DEBUG
+          printf("MAIN -> Se ha producido un error en mq_receive\n");
+          printf("	Oh dear, something went wrong with mq_receive()! %s\n", strerror(errno));
+        #endif
+        mq_unlink(argv[1]);
+        mq_close(mqd);
+        exit(-1);
+      }
+      printf("---Consumidor: %s\n",buffer);
+      memset(buffer,0,sizeof(buffer));
+    }
+    free(buffer);
+    mq_close(mqd);
+  }
+  else
+  {
+    /* Soy el padre - Publisher*/
+    #if DEBUG
+      printf("Productor pid %d \n",getpid());
+    #endif
+    int env;
+    for (i=2;i<argc;i++)
+    {
+    	#if DEBUG
+    		printf("	El valor del tamaño de %s es %d\n",argv[i],sizeof(argv[i]));
+    	#endif
+
+      env = mq_send(mqd,argv[i],sizeof(argv[i]),0);
+
+      if (env == -1)
+      {
+        #if DEBUG
+          printf("MAIN -> Se ha producido un error en mq_send\n");
+          printf("	Oh dear, something went wrong with mq_send()! %s\n", strerror(errno));
+        #endif
+        mq_unlink(argv[1]);
+        mq_close(mqd);
+        exit(-1);
+      }
+      printf("Productor: %s\n",argv[i]);
+      sleep(2);
+    }
+    mq_unlink(argv[1]);
+    mq_close(mqd);
+
+    return 0;
+
+  }
+
+
+
+
+
+
+}
